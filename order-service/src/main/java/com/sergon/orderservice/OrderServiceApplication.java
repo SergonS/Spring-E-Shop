3edce39ae9731e707cc8b477290ce0548e9cddd3
Sergio González <sgzzsifuentes@gmail.com>
2024-02-29 17:40:42 -0600

    Initialized Order Service

---
+++ order-service/src/main/java/com/sergon/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sergon.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
